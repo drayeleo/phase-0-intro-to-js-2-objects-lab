5e8298e91bb63d86cc07e5d54bc8b33c27d608d7
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-04-06 11:38:47 -0600

    commit tree 38dcb56e629ea6f339e74cd69262c8de6705fd1b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = { //may need to be let
+    name: 'Joe Schmo',
+    Address: '456 Nowhere',
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+
+    delete newObj[key];
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+}
